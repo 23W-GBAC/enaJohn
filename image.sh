9fec108503d0c3575ff553d12f2b32a0e61984fb
enajo <enajohnegbe@gmail.com>
2024-01-22 13:31:33 +0100

    North.png

--- image.sh
+++ image.sh
@@ -14,7 +14,7 @@ SRC="/mnt/c/Users/Ena John Egbe/Downloads/$FILENAME"
 DEST="~/enaJohn/images/"
 
 # Copy the file
-cp "$SRC" $DEST
+cp "$SRC" "$DEST"
 
 # Navigate to your Git repository
 cd ~/enaJohn
