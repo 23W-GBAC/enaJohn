8a1e310c362c4bad9eefe27913f9d671f63b4dd8
Oscar Kyamuwendo <148862985+Oscarkyamuwendo@users.noreply.github.com>
2024-02-05 20:44:36 +0100

    changed the indentation of email.sh

--- email.sh
+++ email.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Set the subject and body of the email
+   # Set the subject and body of the email
 SUBJECT="New Blog Post Published!"
 BODY="A new post has been published on my blog. Check it out at: [https://github.com/23W-GBAC/enaJohn/tree/main]"
 
-# Loop through the list of subscribers and send email to each one
+   # Loop through the list of subscribers and send email to each one
 while IFS= read -r email; do
 	    # Send the email using the 'mail' command
 	        echo "$BODY" | mail -s "$SUBJECT" "$email"
